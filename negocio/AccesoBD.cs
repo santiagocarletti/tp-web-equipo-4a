bdd485954e3fad0f6fa357f5b2e8e024001f567b
Nicolas Fernandezn <fernandeznn408@gmail.com>
2025-09-25 01:22:10 -0300

    Agregado de la master y modificacion en la conexion de la BD

--- negocio/AccesoBD.cs
+++ negocio/AccesoBD.cs
@@ -20,7 +20,7 @@ namespace negocio
         }
         public AccesoBD()
         {
-            conexionbd = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            conexionbd = new SqlConnection("server=.\\SQLEXPRESS; database=PROMOS_DB; integrated security=true");
             comando = new SqlCommand();
         }
         public void setearConsulta(string consulta)
